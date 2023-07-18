d483a08f53c04cda44daea703454da6f70d78777
darsh2507 <darshp958@gmail.com>
2023-07-18 20:05:49 +1000

    express server
    -added an api to perform two number operation
    -node project

---
+++ script.js
@@ -0,0 +1,40 @@
+let express = require('express')
+let app = express()
+let port = process.env.port || 5000;
+
+app.use(express.static(__dirname +'/'));
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+
+    res.render('index.html');
+})
+
+app.get("/addTwoNumbers",(req,res)=>{
+
+    let statusCode=200;
+    let successMessage = "successful";
+    let val1  = req.query.val1;
+    let val2 = req.query.val2;
+    let result = parseFloat(val1) + parseFloat(val2);
+    res.json({
+        message:successMessage,
+        code:statusCode,
+        data:result
+    });
+
+});
+
+
+app.post('/multiply', (req, res) => {
+    
+  const { num1, num2 } = req.body;
+    const result = num1 * num2;
+    res.json({ result });
+  
+});
+
+app.use(express.json())
+app.listen(port,()=>{
+    console.log('Server started')
+})
